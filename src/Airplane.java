2ec2413427ff0874abf559e25a1a0a06a93bbce5
Nehir Bozdogan <nehir.bozdgn@gmail.com>
2026-01-24 19:57:28 -0500

    Airplane new version

--- src/Airplane.java
+++ src/Airplane.java
@@ -1,32 +1,31 @@
 import java.io.Serializable;
 
-public class Airplane implements Serializable {
+class Airplane implements Serializable {
     private String model;
     private int seats;
-    private int rangeNm;
+    private String sizeCategory; // NEW: "light_jet", "mid_sized_jet", etc.
     private double cruiseSpeedMach;
     private double purchasePriceUSD;
     private double hourlyOperatingCostUSD;
 
-    // Constructor
-    public Airplane(String model, int seats, int rangeNm, double cruiseSpeedMach, double purchasePriceUSD, double hourlyOperatingCostUSD) {
+    public Airplane(String model, int seats, String sizeCategory, double cruiseSpeedMach,
+                    double purchasePriceUSD, double hourlyOperatingCostUSD) {
         this.model = model;
         this.seats = seats;
-        this.rangeNm = rangeNm;
+        this.sizeCategory = sizeCategory;
         this.cruiseSpeedMach = cruiseSpeedMach;
         this.purchasePriceUSD = purchasePriceUSD;
         this.hourlyOperatingCostUSD = hourlyOperatingCostUSD;
     }
 
-    // Getters & Setters
     public String getModel() { return model; }
     public void setModel(String model) { this.model = model; }
 
     public int getSeats() { return seats; }
     public void setSeats(int seats) { this.seats = seats; }
 
-    public int getRangeNm() { return rangeNm; }
-    public void setRangeNm(int rangeNm) { this.rangeNm = rangeNm; }
+    public String getSizeCategory() { return sizeCategory; }
+    public void setSizeCategory(String sizeCategory) { this.sizeCategory = sizeCategory; }
 
     public double getCruiseSpeedMach() { return cruiseSpeedMach; }
     public void setCruiseSpeedMach(double cruiseSpeedMach) { this.cruiseSpeedMach = cruiseSpeedMach; }
@@ -35,33 +34,32 @@ public class Airplane implements Serializable {
     public void setPurchasePriceUSD(double purchasePriceUSD) { this.purchasePriceUSD = purchasePriceUSD; }
 
     public double getHourlyOperatingCostUSD() { return hourlyOperatingCostUSD; }
-    public void setHourlyOperatingCostUSD(double hourlyOperatingCostUSD) { this.hourlyOperatingCostUSD = hourlyOperatingCostUSD; }
-
-    // String representation
-    @Override
-   public String toString(){
-        return "-----------------------------------------------\n"
-                + "Model : " + model + "\n"
-                + "Seats : " + seats + "\n"
-                + "Range (N.M.): " + rangeNm + "\n"
-                + "Cruise Speed Mach : " + cruiseSpeedMach + "\n"
-                + "Purchase Price (USD) : " + purchasePriceUSD + "\n"
-                + "Hourly Operating Cost (USD) : " + hourlyOperatingCostUSD + "\n";
+    public void setHourlyOperatingCostUSD(double hourlyOperatingCostUSD) {
+        this.hourlyOperatingCostUSD = hourlyOperatingCostUSD;
     }
 
-    // Equals method
+    @Override
+    public String toString() {
+        return "-----------------------------------------------\n"
+                + "Model: " + model + "\n"
+                + "Seats: " + seats + "\n"
+                + "Size Category: " + sizeCategory + "\n"
+                + "Cruise Speed (Mach): " + cruiseSpeedMach + "\n"
+                + "Purchase Price (USD): $" + String.format("%,.2f", purchasePriceUSD) + "\n"
+                + "Hourly Operating Cost (USD): $" + String.format("%,.2f", hourlyOperatingCostUSD) + "\n";
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
 
         Airplane other = (Airplane) obj;
-
         return this.seats == other.seats
-                && this.rangeNm == other.rangeNm
+                && this.sizeCategory.equals(other.sizeCategory)
                 && this.cruiseSpeedMach == other.cruiseSpeedMach
                 && this.purchasePriceUSD == other.purchasePriceUSD
                 && this.hourlyOperatingCostUSD == other.hourlyOperatingCostUSD
                 && this.model.equals(other.model);
-    }//
+    }
 }
